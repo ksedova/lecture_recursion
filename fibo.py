7affe633d2426104da2968034dd24b189819be9d
Karolina Sedova <sedovakarolina@gmail.com>
2023-04-24 12:32:44 +0200

    rucursive_nth_fibo function

--- fibo.py
+++ fibo.py
@@ -1,6 +1,20 @@
+def recursive_nth_fibo(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(num-1) + recursive_nth_fibo(num - 2)
+
+
+
+
+
 
 def main():
-    pass
+   num = int(input("pocet clenu FP"))
+   seg = [recursive_nth_fibo(num) for num in range(num + 1)]
+   print(seg)
 
 
 if __name__ == "__main__":
